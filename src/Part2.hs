e305648cac179d444d7b4b4996a351dea5ad0230
Vladislav <vladalekseev9@yandex.ru>
2020-12-25 10:08:21 +0300

    :zap: some fix in part2 and complete part3

--- src/Part2.hs
+++ src/Part2.hs
@@ -2,29 +2,34 @@ module Part2 where
 
 import Part2.Types
 
+import Data.Function ((&))
 ------------------------------------------------------------
 -- PROBLEM #6
 --
 -- Написать функцию, которая преобразует значение типа
 -- ColorLetter в символ, равный первой букве значения
 prob6 :: ColorLetter -> Char
-prob6 = error "Implement me!"
-
+prob6 RED = 'R'
+prob6 GREEN = 'G'
+prob6 BLUE = 'B'
 ------------------------------------------------------------
 -- PROBLEM #7
 --
 -- Написать функцию, которая проверяет, что значения
 -- находятся в диапазоне от 0 до 255 (границы входят)
 prob7 :: ColorPart -> Bool
-prob7 = error "Implement me!"
-
+prob7 colorPart = getIntFromColorPart >= 0 && getIntFromColorPart <= 255
+    where getIntFromColorPart = prob9 colorPart
 ------------------------------------------------------------
 -- PROBLEM #8
 --
 -- Написать функцию, которая добавляет в соответствующее
 -- поле значения Color значение из ColorPart
 prob8 :: Color -> ColorPart -> Color
-prob8 = error "Implement me!"
+prob8 color colorPart = case colorPart of
+  Red x -> color {red = red color + x}
+  Green x -> color {green = green color + x}
+  Blue x -> color {blue = blue color + x}
 
 ------------------------------------------------------------
 -- PROBLEM #9
@@ -32,23 +37,45 @@ prob8 = error "Implement me!"
 -- Написать функцию, которая возвращает значение из
 -- ColorPart
 prob9 :: ColorPart -> Int
-prob9 = error "Implement me!"
-
+prob9 colorPart = case colorPart of
+    Red int   -> int
+    Green int -> int
+    Blue int  -> int
 ------------------------------------------------------------
 -- PROBLEM #10
 --
 -- Написать функцию, которая возвращает компонент Color, у
 -- которого наибольшее значение (если такой единственный)
-prob10 :: Color -> Maybe ColorPart
-prob10 = error "Implement me!"
+colorPartToInt :: ColorPart -> Int
+colorPartToInt color = prob9 color
 
+maxBy :: Ord a => (t -> a) -> [t] -> t
+maxBy f (x:xs) = iter x xs
+  where
+    iter acc [] = acc
+    iter acc (x:xs) = if f acc < f x then iter x xs else iter acc xs
+
+decompose :: Color -> [ColorPart]
+decompose (Color r g b) = [Red r, Green g, Blue b]
+
+prob10 :: Color -> Maybe ColorPart
+prob10 color = if length maxs == 1 then Just maxValue else Nothing
+  where
+    colors = decompose color
+    maxValue = maxBy colorPartToInt colors
+    maxs = filter (== colorPartToInt maxValue) (map colorPartToInt colors)
 ------------------------------------------------------------
 -- PROBLEM #11
 --
 -- Найти сумму элементов дерева
 prob11 :: Num a => Tree a -> a
-prob11 = error "Implement me!"
+prob11 tree = sum (treeToList tree)
 
+treeToList :: Tree a -> [a]
+treeToList tree = maybeToList (left tree) ++ [root tree] ++ maybeToList (right tree)
+  where
+    maybeToList (Just x) = treeToList x
+    maybeToList Nothing = []
 ------------------------------------------------------------
 -- PROBLEM #12
 --
@@ -58,7 +85,19 @@ prob11 = error "Implement me!"
 -- а все элементы правого поддерева -- не меньше элемента
 -- в узле)
 prob12 :: Ord a => Tree a -> Bool
-prob12 = error "Implement me!"
+prob12 tree = checkTree tree
+  where
+    checkTree :: Ord a => Tree a -> Bool
+    checkTree tree = checkLeft (left tree) (root tree) && checkRight (right tree) (root tree)
+      where
+        checkRight :: Ord a => Maybe (Tree a) -> a -> Bool
+        checkRight Nothing x = True
+        checkRight (Just tree) parent = root tree >= parent && checkTree tree
+
+        checkLeft :: Ord a => Maybe (Tree a) -> a -> Bool
+        checkLeft Nothing x = True
+        checkLeft (Just tree) parent = root tree < parent && checkTree tree
+ 
 
 ------------------------------------------------------------
 -- PROBLEM #13
@@ -67,7 +106,21 @@ prob12 = error "Implement me!"
 -- поддерево, в корне которого находится значение, если оно
 -- есть в дереве поиска; если его нет - вернуть Nothing
 prob13 :: Ord a => a -> Tree a -> Maybe (Tree a)
-prob13 = error "Implement me!"
+prob13 val tree = findTree val (Just tree)
+  where
+    findTree :: Ord a => a -> Maybe (Tree a) -> Maybe (Tree a)
+    findTree _ Nothing = Nothing
+    findTree val (Just tree)
+      | val > root tree = findTree val (right tree)
+      | val < root tree = findTree val (left tree)
+      | otherwise = Just tree
+
+findTree :: Ord a => a -> Maybe (Tree a) -> Maybe (Tree a)
+findTree a Nothing = Nothing
+findTree a (Just tree)
+  | a > root tree = findTree a (right tree)
+  | a < root tree = findTree a (left tree)
+  | otherwise = Just tree
 
 ------------------------------------------------------------
 -- PROBLEM #14
@@ -75,7 +128,29 @@ prob13 = error "Implement me!"
 -- Заменить () на числа в порядке обхода "правый, левый,
 -- корень", начиная с 1
 prob14 :: Tree () -> Tree Int
-prob14 = error "Implement me!"
+prob14 unitTree = traverseTree (getNodesCount unitTree) unitTree
+    where
+        traverseTree :: Int -> Tree () -> Tree Int
+        traverseTree nodeNumber tree = Tree
+            (do
+                leftSubTree <- tree & left
+                return $ traverseTree (pred nodeNumber) leftSubTree)
+            nodeNumber
+            (do
+                rightSubTree <- tree & right
+                return $ traverseTree (getRightDecrementFunc tree nodeNumber) rightSubTree)
+
+        getRightDecrementFunc :: Tree a -> (Int -> Int)
+        getRightDecrementFunc tree = case tree & left of
+            Just leftSubTree -> subtract (getNodesCount leftSubTree + 1)
+            Nothing -> pred
+
+        getNodesCount :: Tree a -> Int
+        getNodesCount tree = succ $ sum
+            [
+                maybe 0 getNodesCount (tree & left),
+                maybe 0 getNodesCount (tree & right)
+            ]
 
 ------------------------------------------------------------
 -- PROBLEM #15
@@ -83,7 +158,11 @@ prob14 = error "Implement me!"
 -- Выполнить вращение дерева влево относительно корня
 -- (https://en.wikipedia.org/wiki/Tree_rotation)
 prob15 :: Tree a -> Tree a
-prob15 = error "Implement me!"
+prob15 tree = maybe tree rotateLeft (right tree)
+    where
+        rotateLeft q = q { left = Just oldRoot }
+           where
+               oldRoot = tree { right = left q }
 
 ------------------------------------------------------------
 -- PROBLEM #16
@@ -91,7 +170,11 @@ prob15 = error "Implement me!"
 -- Выполнить вращение дерева вправо относительно корня
 -- (https://en.wikipedia.org/wiki/Tree_rotation)
 prob16 :: Tree a -> Tree a
-prob16 = error "Implement me!"
+prob16 tree = maybe tree rotateRight (left tree)
+  where
+    rotateRight subTree = subTree {right = Just oldTree}
+      where
+        oldTree = tree {left = right subTree}
 
 ------------------------------------------------------------
 -- PROBLEM #17
@@ -100,4 +183,16 @@ prob16 = error "Implement me!"
 -- разница высот поддеревьев не превосходила по модулю 1
 -- (например, преобразовать в полное бинарное дерево)
 prob17 :: Tree a -> Tree a
-prob17 = error "Implement me!"
+prob17 tree = case buildBalancedTree (treeToList tree) of
+                   Just a -> a
+                   Nothing -> tree
+ 
+buildBalancedTree :: [a] -> Maybe (Tree a)
+buildBalancedTree [] = Nothing
+buildBalancedTree elts =
+  Just (Tree
+    (buildBalancedTree $ take half elts)
+    (elts !! half)
+    (buildBalancedTree $ drop (half + 1) elts))
+  where
+    half = length elts `quot` 2 
--- src/Part3.hs
+++ src/Part3.hs
@@ -1,11 +1,22 @@
 module Part3 where
 
+import Data.List (find, group, nub, sort)
 ------------------------------------------------------------
 -- PROBLEM #18
 --
 -- Проверить, является ли число N простым (1 <= N <= 10^9)
 prob18 :: Integer -> Bool
-prob18 = error "Implement me!"
+prob18 n = getPrimeDivisors n == [n]
+
+getPrimeDivisors :: Integer -> [Integer]
+getPrimeDivisors = currentDivisors 2
+  where
+    currentDivisors :: Integer -> Integer -> [Integer]
+    currentDivisors _ 1 = []
+    currentDivisors d num
+        | d * d > num = [num]
+        | num `mod` d == 0 = d : currentDivisors d (num `div` d)
+        | otherwise = currentDivisors (d + 1) num        
 
 ------------------------------------------------------------
 -- PROBLEM #19
@@ -14,7 +25,10 @@ prob18 = error "Implement me!"
 -- разложении числа N (1 <= N <= 10^9). Простые делители
 -- должны быть расположены по возрастанию
 prob19 :: Integer -> [(Integer, Int)]
-prob19 = error "Implement me!"
+prob19 number = map (\divisors -> (head divisors, length divisors)) groupEqualDivisors
+    where
+        groupEqualDivisors :: [[Integer]]
+        groupEqualDivisors = group (getPrimeDivisors number)
 
 ------------------------------------------------------------
 -- PROBLEM #20
@@ -22,16 +36,25 @@ prob19 = error "Implement me!"
 -- Проверить, является ли число N совершенным (1<=N<=10^10)
 -- Совершенное число равно сумме своих делителей (меньших
 -- самого числа)
-prob20 :: Integer -> Bool
-prob20 = error "Implement me!"
+getDivisors :: Integer -> [Integer]
+getDivisors 1 = [1]
+getDivisors n = (1 :) $ nub $ concat [[x, n `div` x] | x <- [2 .. limit], n `rem` x == 0]
+  where
+    limit = (floor . sqrt . fromIntegral) n
 
+prob20 :: Integer -> Bool
+prob20 1 = False
+prob20 n = n == sum (getDivisors n)
+-- prob20 1 = False
 ------------------------------------------------------------
 -- PROBLEM #21
 --
 -- Вернуть список всех делителей числа N (1<=N<=10^10) в
 -- порядке возрастания
 prob21 :: Integer -> [Integer]
-prob21 = error "Implement me!"
+prob21 1 = [1]
+prob21 n = (sort . getDivisors) n ++ [n]
+-- prob21 1 = [1]
 
 ------------------------------------------------------------
 -- PROBLEM #22
@@ -39,7 +62,10 @@ prob21 = error "Implement me!"
 -- Подсчитать произведение количеств букв i в словах из
 -- заданной строки (списка символов)
 prob22 :: String -> Integer
-prob22 = error "Implement me!"
+prob22 input = product $ (map lettersCount) (words input)
+    where
+        lettersCount :: String -> Integer
+        lettersCount word = toInteger $ length (filter (=='i') word)
 
 ------------------------------------------------------------
 -- PROBLEM #23
@@ -50,7 +76,30 @@ prob22 = error "Implement me!"
 -- M > 0 и N > 0. Если M > N, то вернуть символы из W в
 -- обратном порядке. Нумерация символов с единицы.
 prob23 :: String -> Maybe String
-prob23 = error "Implement me!"
+prob23 inputString = return inputString >>= parseInput >>= getSlice
+    where
+        parseInput :: String -> Maybe ParseResult
+        parseInput input = do
+            let left = read $ takeWhile (/= '-') input
+            let right = read $ takeWhile (/= ':') $ tail $ dropWhile (/= '-') input
+            let string = tail $ dropWhile (/= ' ') input
+            return ParseResult { leftBound = left, rightBound = right, stringToSlice = string }
+
+        getSlice :: ParseResult -> Maybe String
+        getSlice (ParseResult left right string)
+            | left > length string || right > length string = Nothing
+            | right >= left = Just $ leftToRightSlice left right
+            | otherwise = Just $ reverse $ leftToRightSlice right left
+            where
+                leftToRightSlice :: Int -> Int -> String
+                leftToRightSlice l r = take r $ drop (l - 1) string
+
+data ParseResult = ParseResult
+    {
+        leftBound :: Int,
+        rightBound :: Int,
+        stringToSlice :: String
+    }
 
 ------------------------------------------------------------
 -- PROBLEM #24
@@ -59,7 +108,9 @@ prob23 = error "Implement me!"
 -- представить как сумму чисел от 1 до какого-то K
 -- (1 <= N <= 10^10)
 prob24 :: Integer -> Bool
-prob24 = error "Implement me!"
+prob24 num = checkNum(sqrt (1 + 8 * fromInteger num)) == 0
+  where
+    checkNum x = x - fromIntegral (floor x)
 
 ------------------------------------------------------------
 -- PROBLEM #25
@@ -67,7 +118,9 @@ prob24 = error "Implement me!"
 -- Проверить, что запись числа является палиндромом (т.е.
 -- читается одинаково слева направо и справа налево)
 prob25 :: Integer -> Bool
-prob25 = error "Implement me!"
+prob25 = isPalindrome . show
+  where
+    isPalindrome xs = xs == reverse xs
 
 ------------------------------------------------------------
 -- PROBLEM #26
@@ -76,7 +129,7 @@ prob25 = error "Implement me!"
 -- сумма делителей одного (без учёта самого числа) равна
 -- другому, и наоборот
 prob26 :: Integer -> Integer -> Bool
-prob26 = error "Implement me!"
+prob26 left right = (sum . getDivisors) left == right && (sum . getDivisors) right == left
 
 ------------------------------------------------------------
 -- PROBLEM #27
@@ -84,7 +137,10 @@ prob26 = error "Implement me!"
 -- Найти в списке два числа, сумма которых равна заданному.
 -- Длина списка не превосходит 500
 prob27 :: Int -> [Int] -> Maybe (Int, Int)
-prob27 = error "Implement me!"
+prob27 _ [] = Nothing
+prob27 s (x:xs) = if s - x `elem` xs 
+                    then Just (x, s - x)
+                    else prob27 s xs
 
 ------------------------------------------------------------
 -- PROBLEM #28
@@ -93,15 +149,36 @@ prob27 = error "Implement me!"
 -- заданному.
 -- Длина списка не превосходит 500
 prob28 :: Int -> [Int] -> Maybe (Int, Int, Int, Int)
-prob28 = error "Implement me!"
+prob28 requiredSum inputList = do
+    list <- find
+            (\list -> sum list == requiredSum)
+            $ subsets 4 inputList
+    return (list !! 3, list !! 2, list !! 1, list !! 0)
 
+    where
+        subsets :: Int -> [a] -> [[a]]
+        subsets subLength listToHandle =
+            if subLength > length listToHandle
+            then []
+            else subsequencesBySize listToHandle !! (length listToHandle - subLength)
+
+        subsequencesBySize [] = [[[]]]
+        subsequencesBySize (curHead : curTail) =
+            let next = subsequencesBySize curTail
+            in zipWith (++) ([] : next) (map (map (curHead :)) next ++ [[]])
 ------------------------------------------------------------
 -- PROBLEM #29
 --
 -- Найти наибольшее число-палиндром, которое является
 -- произведением двух K-значных (1 <= K <= 3)
 prob29 :: Int -> Int
-prob29 k = error "Implement me!"
+prob29 k = maximum [x * y|
+    x <- [minNumber..maxNumber],
+    y <-[minNumber..maxNumber],
+    (prob25 . toInteger) (x*y)]
+      where
+        minNumber = 10 ^ (k - 1)
+        maxNumber = 10 ^ k - 1
 
 ------------------------------------------------------------
 -- PROBLEM #30
@@ -109,7 +186,18 @@ prob29 k = error "Implement me!"
 -- Найти наименьшее треугольное число, у которого не меньше
 -- заданного количества делителей
 prob30 :: Int -> Integer
-prob30 = error "Implement me!"
+prob30 reqCount = head $
+    filter (\triangular -> (length . getAllDivisors) triangular >= reqCount)
+    triangularNumbers
+      where
+          getAllDivisors :: Integer -> [Integer]
+          getAllDivisors n = (if n == 0 then [] else [n]) ++ getDivisors n
+
+triangularNumbers :: [Integer]
+triangularNumbers = triangularWithCurrent 0 1
+     where
+           triangularWithCurrent :: Integer -> Integer -> [Integer]
+           triangularWithCurrent current next = current : triangularWithCurrent (current + next) (succ next)       
 
 ------------------------------------------------------------
 -- PROBLEM #31
@@ -117,7 +205,11 @@ prob30 = error "Implement me!"
 -- Найти сумму всех пар различных дружественных чисел,
 -- меньших заданного N (1 <= N <= 10000)
 prob31 :: Int -> Int
-prob31 = error "Implement me!"
+prob31 n = sum [
+                x + y |x <- [1 .. n],
+                y <- [x+1 .. n],
+                prob26 (toInteger x) (toInteger y)
+            ]
 
 ------------------------------------------------------------
 -- PROBLEM #32
@@ -127,4 +219,11 @@ prob31 = error "Implement me!"
 -- указанного достоинства
 -- Сумма не превосходит 100
 prob32 :: [Int] -> Int -> [[Int]]
-prob32 = error "Implement me!"
+prob32 coins moneySum
+    | moneySum < minimum coins = []
+    | otherwise = [
+            coin : nextCoins |
+            coin <- reverse coins,
+            nextCoins <- [] : prob32 (filter (<= coin) coins) (moneySum - coin),
+            sum (coin : nextCoins) == moneySum
+        ]
